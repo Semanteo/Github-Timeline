a7fd3fca87bec5f197d6d3218a0a9b5bc4850f71
Semanteo <simsim3374@gmail.com>
2022-08-27 09:59:01 +0200

    Update logos plus languages plus values
    Adding fork logo and star counter
    
    Adding language name plus color
    
    Adding default values

--- script.js
+++ script.js
@@ -24,19 +24,30 @@ if(getUrlParameter('username') != ''){
           return dateB - dateA
         }
       })
+      fetch("https://raw.githubusercontent.com/ozh/github-colors/master/colors.json").then(async res => {return await res.json()})
+      .then(data => {
       for(var i = 0; i < repos.length; i++) {
         var li = document.createRange().createContextualFragment(`
         <li>
         <div>
-        <time>${repos[i].name} - ${repos[i].created_at.split("T")[0]}</time>
-        ${repos[i].description?repos[i].description:'No description'}<br>
-        ${repos[i].svn_url?`<a id="url" target="__blank" href="${repos[i].svn_url}">Link to repo</a>`:''}
-        </div>
+        <time onmouseover="hoverFunc(${i})" onmouseout="outFunc(${i})" onclick='window.open("${repos[i].html_url}", "__blank")'>${repos[i].fork?`<img id="git-fork-${i}" src="./img/git-fork.svg"/>`:""}${repos[i].name} -  ${repos[i].stargazers_count} <img id="git-star-${i}" src='./img/git-star.svg'/></time>
+        <p style="color: #0d1117;">Created ${repos[i].created_at.split("T")[0]} - Last update ${repos[i].updated_at.split("T")[0]}</p>
+        <p>${repos[i].description?repos[i].description:'No description'}</p>
+        <br>
+        ${repos[i].language?`<label class="container">
+        <span style="background-color:${repos[i].language?data[repos[i].language].color:"#0d1117"};" class="option-input radio"></span>
+        ${repos[i].language} </label>
+        `:' '}
+          </div>
         </li>
         `);
         document.getElementById('timeline').append(li);
-      }
-    })
+      }  
+      }).catch(err => {
+          console.log(err)
+        })
+        
+      })
     .catch(err => {
       alert(`${getUrlParameter('username')} is not a valid username`);
     })
@@ -78,4 +89,14 @@ if(getUrlParameter('username') != ''){
 
 function view_on_github(){
     window.open('https://github.com/Semanteo/Github-Timeline', '__blank');
+}
+
+function hoverFunc(i){
+    if (document.getElementById(`git-star-${i}`)) document.getElementById(`git-star-${i}`).src = "./img/git-star-black.svg";
+    if (document.getElementById(`git-fork-${i}`)) document.getElementById(`git-fork-${i}`).src = "./img/git-fork-black.svg";
+}
+
+function outFunc(i){
+  if (document.getElementById(`git-star-${i}`)) document.getElementById(`git-star-${i}`).src = "./img/git-star.svg";
+  if (document.getElementById(`git-fork-${i}`)) document.getElementById(`git-fork-${i}`).src = "./img/git-fork.svg";
 }
